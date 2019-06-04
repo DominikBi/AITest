d3b06285ec7ae64dc266c1e8c73e9fc19c09d708
DominikBi <birkeldominik@gmail.com>
2019-06-04 17:07:06 +0200

    asd

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,8 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitor;
 import java.util.ArrayList;
 import java.util.logging.FileHandler;
 
@@ -20,15 +22,21 @@ public class Main {
     private int streakRight;
     int i = 1;
     int pastI = 0;
+    String home = System.getProperty("user.home");
+    int maxCharsint;
+    String sep = System.getProperty("file.separator");
 
     String filename;
-
+    JTextArea name = new JTextArea("Filename: ");
+    JTextArea types = new JTextArea("File type: ");
+    JTextArea chars = new JTextArea("Max chars: ");
+    JButton button = new JButton("Find files");
     JButton button1 = new JButton("Left");
     JButton button2 = new JButton("Right");
+    JTextField maxChars = new JTextField("2");
     JTextField textField = new JTextField();
-    JTextField type = new JTextField();
+    JTextField type = new JTextField(".txt");
     JButton jButton = new JButton("Read");
-    private Object ResolverLocalFilesystem;
 
     public static void main(String[] args){
        Main main = new Main();
@@ -110,31 +118,93 @@ public class Main {
 
         }
         public void start() {
+
+        button.addActionListener(e -> {
+            try {
+                maxCharsint = Integer.parseInt( maxChars.getText());
+
+            } catch (NumberFormatException ex) {
+                JFrame popFrame = new JFrame();
+                JPanel popPanel = new JPanel();
+                    popFrame.setSize(260,150);
+                popFrame.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2 - 130,Toolkit.getDefaultToolkit().getScreenSize().height/2-75);
+                JTextField popField = new JTextField();
+                JButton popButton = new JButton("OK");
+                popPanel.setLayout(new BoxLayout(popPanel, BoxLayout.PAGE_AXIS));
+                popField.setText("The maximum char has to be a number!");
+                popField.setEditable(false);
+                popPanel.add(popField);
+                popPanel.add(popButton);
+                popFrame.add(popPanel);
+                popFrame.setVisible(true);
+                popButton.addActionListener(e12 -> {
+                    popFrame.setVisible(false);
+
+
+                });
+            }
+
+
+        String randomPath = "";
+        int i1 = 0;
+        int i2 =0;
+        for(int i = 0; i < maxCharsint; i++) {
+
+            i1 = 0;
+            for (char c3 = 'a'; i1 < 26; c3++) {
+                randomPath += c3;
+                File file = new File(home + sep + randomPath + type.getText());
+
+                if (file.exists()) {
+                    System.out.println("Found: " + randomPath);
+
+
+                }
+                System.out.println(randomPath.charAt(randomPath.length()-1) + " : " + c3);
+                randomPath.replace(randomPath.charAt(randomPath.length()-1),' ');
+                i1++;
+            }
+            System.out.println(randomPath);
+
+        }});
+
+
             JPanel panel = new JPanel();
             JFrame frame = new JFrame();
             filename = "";
+            maxChars.setPreferredSize(new Dimension(250,70));
             textField.setPreferredSize(new Dimension(250, 70));
             type.setPreferredSize(new Dimension(250,70));
             panel.setLayout(new FlowLayout());
+            name.setEditable(false);
+            types.setEditable(false);
+            chars.setEditable(false);
+            panel.add(name);
             panel.add(textField);
+            panel.add(types);
             panel.add(type);
+            panel.add(chars);
+            panel.add(maxChars);
+            panel.add(button);
             panel.add(jButton);
-            ArrayList<Character> al = new ArrayList<Character>();
+            ArrayList<Character> al = new ArrayList<>();
             int i =0;
             for(char c = 'a'; i < 26 ; c++){
                 al.add(c);
                 System.out.println(al.get(i));
                 i++;
             }
-            i=0;
             jButton.addActionListener(e -> {
                 filename = textField.getText();
                 String fileType = type.getText();
+
+
+
                 System.out.println(fileType);
                 try {
                     String home = System.getProperty("user.home");
                     String sep = System.getProperty("file.separator");
-                    System.out.println(home + sep+ filename + ".txt");
+
                     File file = new File(home + sep+ filename + fileType);
 
                     if(file.exists()) {
@@ -151,10 +221,10 @@ public class Main {
 
                         jFrame.add(panel1);
                     }
-                } catch (NullPointerException e1){
+                } catch (NullPointerException e1) {
                     System.err.println(e1);
-                }
 
+                }
             });
 
 
@@ -166,13 +236,12 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-        GraphicsDevice gp = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        gp.setFullScreenWindow(frame);
+        frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
 
 
 
     }
     public double random(int min,int max){
-        return Math.random()*max +(max - min - 1);
+        return Math.random()*(max - min) +min;
     }
 }
